a4f7f99e0f3ba21ee7fad93603c7681292f16f2a
Milton ferney vasquez <fercho0926@hotmail.com>
2024-08-30 20:28:57 -0500

    datatables

--- Web2/src/app/components/user-management/users/users.component.ts
+++ Web2/src/app/components/user-management/users/users.component.ts
@@ -1,25 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { User } from 'app/types/user-types-new';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UserService } from 'app/services/user.service';
 import { MatDialog } from '@angular/material/dialog';
 
+import * as $ from 'jquery'; // Import jQuery
+import 'datatables.net'; // Import DataTables library
+
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit  {
 
 userList: User[] = [];
+
+dataTable: any;
 
 
    constructor(private userService: UserService, private dialog: MatDialog){}
 
-  ngOnInit(): void {
-    console.log("UsersComponent ngOnInit");
-     this.userService.getAllUsers().subscribe(data => this.userList = data);
+   ngOnInit(): void {
+    // Fetch users from the service
+    this.userService.getAllUsers().subscribe(data => {
+      this.userList = data;
+      this.initializeDataTable(); // Initialize DataTable after data is fetched
+    });
   }
 
+
+  initializeDataTable(): void {
+    // Check if DataTable is already initialized and destroy it if so
+    if ($.fn.DataTable.isDataTable('#userTable')) {
+      $('#userTable').DataTable().destroy();
+    }
+
+    // Initialize DataTable
+    this.dataTable = $('#userTable').DataTable({
+      data: this.userList,
+      columns: [
+        { data: 'identification' },
+        {
+          data: 'firstName',
+          render: (data, type, row) => `${data} ${row.lastName}`
+        },
+        { data: 'email' },
+        {
+          data: 'isActive',
+          render: (data, type, row) => {
+            return `<mat-slide-toggle class="toggle-slide" [checked]="${data}" (change)="onToggleChange($event, ${row.id})"></mat-slide-toggle>`;
+          },
+          orderable: false // Make the column non-orderable
+        },
+        {
+          data: null,
+          defaultContent: `
+              <button class="btn btn-icon" matTooltip="View" [matTooltipPosition]="'above'" (click)="viewUser(event)">
+              <i class="material-icons">visibility</i>
+            </button>
+            <button class="btn btn-icon" matTooltip="Edit" [matTooltipPosition]="'above'" (click)="editUser(event)">
+              <i class="material-icons">edit</i>
+            </button>
+            <button class="btn btn-icon" matTooltip="Remove" [matTooltipPosition]="'above'" (click)="removeUser(event)">
+              <i class="material-icons">delete</i>
+            </button>
+          `
+        }
+      ]
+    });
+  }
+
+
+  ngAfterViewInit(): void {
+    // Initialize DataTables after the view is initialized
+    $(document).ready(function() {
+    });
+  } 
+  
+  
 
 
 
