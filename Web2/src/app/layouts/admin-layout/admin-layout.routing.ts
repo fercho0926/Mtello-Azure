c74602e92852520ef7cb11ca99877cbc606caff2
Milton ferney vasquez <fercho0926@hotmail.com>
2024-09-05 17:38:51 -0500

    delete zombie code

--- Web2/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Web2/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,12 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-// import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UserManagementComponent } from 'app/components/user-management/user-management/user-management.component';
 import { ChecksComponent } from 'app/components/checks/checks.component';
 import { ReportingComponent } from 'app/components/reporting/reporting.component';
@@ -21,12 +15,6 @@ export const AdminLayoutRoutes: Routes = [
 
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'checks',      component: ChecksComponent },
-    // { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
     {path: 'user-management', component: UserManagementComponent},
     {path: 'reporting', component: ReportingComponent},
     {path: 'reporting-details/:reportingId', component: ReportingDetailsComponent},
